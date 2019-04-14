242739e0bf6969b6ef9bfa98210051c8216db07c
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-04-14 19:51:43 +0200

    saving surfspots info with scraper

--- lib/api/scraper.rb
+++ lib/api/scraper.rb
@@ -1,26 +1,66 @@
 require 'mechanize'
 
-class Scraper < Mechanize
-  WEBSITE = "https://www.wannasurf.com"
-  INDO = "/spot/Asia/Indonesia"
-
-  def airports
-    get("#{WEBSITE}#{INDO}/Bali/airports/index.html")
+module Scraper
+  class Country
   end
+  class Spot < Mechanize
+    WEBSITE = "https://www.wannasurf.com"
+    INDO = "/spot/Asia/Indonesia"
 
-  def gps
-    airports.link_with(text: "GPS History (1)").click
-  end
+    def airport
+      get("#{WEBSITE}#{INDO}/Bali/airports/index.html")
+    end
 
-  def coordinates
-    page.at("table table[width='100%'] tr:last-child td:first-child").children
-  end
+    def name 
+      airport.search("a.wanna-item-title-title").text
+    end
 
-  def latitude
-    coordinates[0].text
-  end
+    def info
+      airport.search("h3.wanna-item")
+    end
 
-  def longitude
-    coordinates[2].text.lstrip
+    def characteristics
+      info[1].next_element.children.search("p")
+    end
+
+    def additional_information
+      info[2].next_element.text
+    end
+
+    def quality
+      characteristics[0].children[1].text
+    end
+
+    def experience
+      characteristics[1].children[1].text
+    end
+    
+    def frequency
+      characteristics[2].children[1].text
+    end
+
+    def bottom
+      characteristics[3].children[1].text
+    end
+
+    def direction
+      characteristics[4].children[1].text
+    end
+
+    def gps
+      airport.link_with(text: "GPS History (1)").click
+    end
+
+    def coordinates
+      gps.at("table table[width='100%'] tr:last-child td:first-child").children
+    end
+
+    def latitude
+      coordinates[0].text
+    end
+
+    def longitude
+      coordinates[2].text.lstrip
+    end
   end
 end
