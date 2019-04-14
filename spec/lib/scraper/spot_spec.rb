22530ac55c9aca595e61f63226916f515c29ce68
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-04-14 21:50:03 +0200

    deleting code from scraper

--- spec/lib/scraper/spot_spec.rb
+++ spec/lib/scraper/spot_spec.rb
@@ -25,25 +25,5 @@ describe Scraper::Spot do
       subject.set_attributes
       expect(subject.attributes).to eql attributes
     end
-
-    it "#quality" do 
-      expect(subject.quality).to eql "Normal"
-    end
-
-    it "#experience" do
-      expect(subject.experience).to eql "All surfers"
-    end
-
-    it "#frequency" do
-      expect(subject.frequency).to eql "Sometimes break"
-    end
-
-    it "#direction" do
-      expect(subject.direction).to eql "Right and left"
-    end
-
-    it "#bottom" do
-      expect(subject.bottom).to eql "Reef-coral"
-    end
   end
 end
