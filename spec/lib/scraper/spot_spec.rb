f473193355ced7efeb0c230e95976740418b1a28
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-04-15 18:42:06 +0200

    scraper test cases

--- spec/lib/scraper/spot_spec.rb
+++ spec/lib/scraper/spot_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
+require 'scraper'
 require 'scraper/spot'
 
 describe Scraper::Spot do 
-  subject { Scraper::Spot.new("https://www.wannasurf.com/spot/Asia/Indonesia/Bali/airports/index.html") }
+  subject { Scraper::Spot.new("#{Scraper::WEBSITE}/spot/Asia/Indonesia/Bali/airports/index.html") }
+  let(:geger) { Scraper::Spot.new "#{Scraper::WEBSITE}/spot/Asia/Indonesia/Bali/geger/index.html" }
   let(:attributes) {{ direction: "Right and left", experience: "All surfers", frequency: "Sometimes break", type: "Reef-coral", wave_quality: "Normal", name: "Airport's", latitude: "-8.745124", longitude: "115.150323" }}
 
   describe "retrieves spot information" do
     it "#name" do 
       expect(subject.name).to eql "Airport's"
+    end
+
+    it "#gps?" do
+      subject.gps
+      expect(subject.gps?).to be true
+    end
+
+    it "#coordinates" do
+      expect(subject.coordinates[0].text).to eql "-8.745124"
     end
 
     it "#latitude" do
@@ -22,14 +33,14 @@ describe Scraper::Spot do
       expect(subject.additional_information).to match "Airport's right and left need quite a big"
     end
 
-    it "#gps" do
-      expect(subject.gps.title).to match "GPS location"
-      expect(subject.spot).not_to match "GPS location"
-    end
-
     it "#attributes" do 
       subject.set_attributes
       expect(subject.attributes).to match attributes
+    end
+    
+    it "#attributes with layout issues" do
+      expect(geger.attributes[:name]).to eql "Geger"
+      expect(geger.attributes[:experience]).to eql "Experienced surfers"
     end
   end
 end
