731e3f813e236a130c6e4577395fdfa1866ebbda
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-04-16 16:52:08 +0200

    adding pictures to spot

--- spec/lib/scraper/spot_spec.rb
+++ spec/lib/scraper/spot_spec.rb
@@ -3,9 +3,9 @@ require 'scraper'
 require 'scraper/spot'
 
 describe Scraper::Spot do 
-  subject { Scraper::Spot.new("#{Scraper::WEBSITE}/spot/Asia/Indonesia/Bali/airports/index.html") }
-  let(:geger) { Scraper::Spot.new "#{Scraper::WEBSITE}/spot/Asia/Indonesia/Bali/geger/index.html" }
-  let(:attributes) {{ direction: "Right and left", experience: "All surfers", frequency: "Sometimes break", bottom: "Reef-coral", wave_quality: "Normal", name: "Airport's", latitude: "-8.745124", longitude: "115.150323" }}
+  subject { Scraper::Spot.new("#{ENV["WEBSITE"]}/spot/Asia/Indonesia/Bali/airports/index.html") }
+  let(:geger) { Scraper::Spot.new "#{ENV["WEBSITE"]}/spot/Asia/Indonesia/Bali/geger/index.html" }
+  let(:attributes) {{ direction: "Right and left", experience: "All surfers", frequency: "Sometimes break", bottom: "Reef-coral", wave_quality: "Normal", name: "Airport's", latitude: "-8.745124", longitude: "115.150323", pictures: "" }}
 
   describe "retrieves spot information" do
     it "#name" do 
@@ -35,12 +35,20 @@ describe Scraper::Spot do
 
     it "#attributes" do 
       subject.set_attributes
+      expect(subject.attributes[:pictures].size).to be 11
+      subject.attributes[:pictures] = ""
       expect(subject.attributes).to match attributes
     end
     
     it "#attributes with layout issues" do
       expect(geger.attributes[:name]).to eql "Geger"
       expect(geger.attributes[:experience]).to eql "Experienced surfers"
+    end
+
+    it "#pictures" do
+      picture = "#{ENV['WEBSITE']}/spot/Asia/Indonesia/Bali/airports/photo/ico_photo_surf_Indonesia_Bali_airports_451d7ee651d4d.jpg"
+      expect(subject.pictures).to include picture
+      expect(subject.pictures.size).to be 11 
     end
   end
 end
