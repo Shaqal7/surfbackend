af7d9ce3f08bfb3d877270a4fb636a679981611b
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-23 11:26:19 +0800

    scrape an entire continent

--- spec/lib/scraper/continent_spec.rb
+++ spec/lib/scraper/continent_spec.rb
@@ -2,5 +2,14 @@ require 'rails_helper'
 require 'continent'
 
 describe Continent do
-  it "#countries" 
+  let(:continent) { Continent.new("https://www.wannasurf.com/spot/North_America/index.html") }
+  describe '#countries' do
+    it 'returns the country links' do
+      expect(continent.countries).to eql ["/spot/North_America/Canada/index.html", "/spot/North_America/USA/index.html"]
+    end
+  end
+
+  describe '#scrape' 
+    it 'persist the new spots to the database'
+      # expect(continent.scrape).to 
 end
